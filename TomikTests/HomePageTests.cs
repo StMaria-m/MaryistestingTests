a0ac4a87c19c1a2a983eb199aff78c539674219c
StMaria-m <stmaria-m@github>
2021-09-22 12:42:17 +0200

    Add more tests to HomePageTests

--- TomikTests/HomePageTests.cs
+++ TomikTests/HomePageTests.cs
@@ -6,20 +6,45 @@ namespace TomikTests
 {
    public class HomePageTests: BaseTest
     {
-
         [Test]
         [Category("Home page tests")]
         [Author("Maria", "http://maryistesting.com")]
         [Description("Sprawdzenie przejścia na stronę przypomnienie hasła")]
-        public void Test6()
+        public void CorrectGoToLostPasswordPageTest()
         {
-            //1. Kliknąć w przycisk "zapomniałem"
+            //kliknąć w przycisk "zapomniałem"
             var loginButton = _webDriver.FindElement(By.CssSelector(".forgotPass"));
             loginButton.Click();
 
-            //2. Sprawdzenie przekierowania na stronę do przypomnienia hasła
+            //sprawdzenie przekierowania na stronę do przypomnienia hasła
             var url = _webDriver.Url;
             StringAssert.Contains("LostPassword.aspx", url);
+        }
+
+        [Test]
+        [Category("User login  account tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Sprawdzenie działania przycisku Wyloguj")]
+        public void CorrectLogOutTest()
+        {
+            LogInSteps();
+
+            //kliknąć przycisk "Wyloguj"
+            var logoutButton = _webDriver.FindElement(By.CssSelector("#logout"));
+            logoutButton.Click();
+
+            try
+            {
+                //sprawdzenie czy pojawi się przycisk "Zaloguj", (jeśli tak, to wylogowanie działa poprawnie)
+                _webDriver.FindElement(By.CssSelector("#topBar_LoginBtn"));
+            }
+            catch (NoSuchElementException)
+            {
+                Assert.Fail("Button not found");
+                return;
+            }
+
+            Assert.Pass();
         }
     }
 }
