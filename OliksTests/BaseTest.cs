3ef8697a77d2da33cef9981a8aba9c2774a19019
StMaria-m <stmaria-m@github>
2021-09-25 22:12:15 +0200

    Add project OliksTests and LogInTests

---
+++ OliksTests/BaseTest.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
+using System.Threading;
+using OliksTests.Enums;
+
+namespace OliksTests
+{
+    public class BaseTest
+    {
+        protected IWebDriver _webDriver;
+        protected string _path;
+
+        [SetUp]
+        public void Open()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Url = $"https://https://www.sendit.pl/{_path}";
+
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            _webDriver.Quit();
+        }
+
+        public void ClickAcceptContainer()
+        {
+            var agreement = _webDriver.FindElement(By.CssSelector("#onetrust-accept-btn-handler"));
+            agreement.Click();
+        }
+
+       
+        public void WaitForAction(string selector, SearchByTypeEnums selectorType = SearchByTypeEnums.CssSelector)
+        {
+            var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => {
+                var selectorFunction = selectorType == SearchByTypeEnums.CssSelector ? By.CssSelector(selector) : By.XPath(selector);
+
+                var element = driver.FindElements(selectorFunction).FirstOrDefault();
+                if (element == null)
+                {
+                    return false;
+                }
+                return element.Displayed;
+            });
+        }
+
+        //olx
+        public void LogInSteps(string path = null)
+        {
+            ClickAcceptContainer();
+
+            //kliknąć element "Mój OLX"
+            var loginLink = _webDriver.FindElement(By.CssSelector("#topLoginLink"));
+            loginLink.Click();
+
+            WaitForAction("#loginForm");
+
+            //uzupełnić "E-mail do konta OLX"
+            var userEmailInput = _webDriver.FindElement(By.CssSelector("#userEmail"));
+            userEmailInput.SendKeys("m.test@o2.pl");
+
+            //uzupełnić hasło
+            var userPasswordInput = _webDriver.FindElement(By.CssSelector("#userPass"));
+            userPasswordInput.SendKeys("123456^%$#@!Qa");
+
+            //kliknać "Zaloguj się"
+            var logInButton = _webDriver.FindElement(By.CssSelector("#se_userLogin"));
+            logInButton.Click();
+
+            WaitForAction("#se_accountShop");
+
+            if (path != null)
+            {
+                _webDriver.Navigate().GoToUrl($"https://www.olx.pl/{path}");
+            }
+        }
+    }
+}
---
+++ OliksTests/LogInTests.cs
@@ -0,0 +1,182 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace OliksTests
+{
+    public class LogInTests : BaseTest
+    {
+        private string _myAccountLinkSelector = "#topLoginLink";
+        private string _logInFormSelector = "#loginForm";
+        private string _errorMessageSelector = "#se_emailError";
+        private string _loginFormErrorMessageSellector = "#loginForm > div.errorbox > p";
+        private string _logInButtonSelector = "#se_userLogin";
+        private string _userLoginInputSelector = "#userEmail";
+        private string _userPasswordInputSelector = "#userPass";
+
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Poprawne logowanie na konto użytkownika")]
+        public void CorrectLogInTest()
+        {
+            LogInSteps();
+
+            try
+            {
+                //Sprawdzenie czy użytkownik został zalogowany
+                _webDriver.FindElement(By.CssSelector("#se_accountShop"));
+            }
+            catch (OpenQA.Selenium.NoSuchElementException)
+            {
+                Assert.Fail("User is not log in");
+                return;
+            }
+
+            Assert.Pass();
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Brak loginu i hasła - niepoprawne logowanie na konto użytkownika")]
+        public void IncorrectLog_noLoginAndPasswordTest()
+        {
+            ClickAcceptContainer();
+
+            ClickMyAccount();
+
+            WaitForAction(_logInFormSelector);
+
+            //kliknać "Zaloguj się"
+            var logInButton = _webDriver.FindElement(By.CssSelector(_logInButtonSelector));
+            logInButton.Click();
+
+            //sprawdzić, czy pojawi się komunikat "To pole jest wymagane"
+            var emptyPasswordInput = _webDriver.FindElement(By.CssSelector(_errorMessageSelector));
+            StringAssert.Contains("To pole jest wymagane", emptyPasswordInput.Text);
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Błędny format e-maila - niepoprawne logowanie na konto użytkownika")]
+        public void IncorrectLogIn_wrongLoginTest()
+        {
+            string userMail = "23423423test";
+
+            ClickAcceptContainer();
+
+            ClickMyAccount();
+
+            WaitForAction(_logInFormSelector);
+
+            //uzupełnić "E-mail do konta OLX"
+            var userEmailInput = _webDriver.FindElement(By.CssSelector(_userLoginInputSelector));
+            userEmailInput.SendKeys(userMail);
+
+            _webDriver.FindElement(By.TagName("body")).Click();
+
+            WaitForAction(_errorMessageSelector);
+
+            //sprawdzić, czy pojawi się komunikat "Niepoprawny format e-mail"
+            var emptyPasswordInput = _webDriver.FindElement(By.CssSelector(_errorMessageSelector));
+            StringAssert.Contains("Niepoprawny format e-mail", emptyPasswordInput.Text);
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Błędny login o poprawnym formacie i hasło - niepoprawne logowanie na konto użytkownika")]
+        public void IncorrectLogIn_wrongLoginAndPasswordTest()
+        {
+            string userMail = "123abc@vp.pl";
+            string userPassword = "abcdef";
+
+            ClickAcceptContainer();
+
+            ClickMyAccount();
+
+            WaitForAction(_logInFormSelector);
+
+            //uzupełnić "E-mail do konta OLX"
+            var userEmailInput = _webDriver.FindElement(By.CssSelector(_userLoginInputSelector));
+            userEmailInput.SendKeys(userMail);
+
+            //uzupełnić hasło
+            var userPasswordInput = _webDriver.FindElement(By.CssSelector(_userPasswordInputSelector));
+            userPasswordInput.SendKeys(userPassword);
+
+            //kliknać "Zaloguj się"
+            var logInButton = _webDriver.FindElement(By.CssSelector(_logInButtonSelector));
+            logInButton.Click();
+
+            WaitForAction(_loginFormErrorMessageSellector);
+
+            //sprawdzić, czy pojawi się komunikat "Nieprawidłowy login lub hasło"
+            var logInError = _webDriver.FindElement(By.CssSelector(_loginFormErrorMessageSellector));
+            StringAssert.Contains("Nieprawidłowy login lub hasło", logInError.Text);
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Błędna domena e-maila - niepoprawne logowanie na konto użytkownika")]
+        public void IncorrectLogIn_wrongEmailDomainTest()
+        {
+            string usermail = "123abc@az.com";
+            string password = "1234zaqz";
+
+            ClickAcceptContainer();
+
+            ClickMyAccount();
+
+            WaitForAction(_logInFormSelector);
+
+            //uzupełnić "E-mail do konta OLX"
+            var userEmailInput = _webDriver.FindElement(By.CssSelector(_userLoginInputSelector));
+            userEmailInput.SendKeys(usermail);
+
+            //uzupełnić hasło
+            var userPasswordInput = _webDriver.FindElement(By.CssSelector(_userPasswordInputSelector));
+            userPasswordInput.SendKeys(password);
+
+            //kliknać "Zaloguj się"
+            var logInButton = _webDriver.FindElement(By.CssSelector(_logInButtonSelector));
+            logInButton.Click();
+
+            WaitForAction(_errorMessageSelector);
+
+            //sprawdzić, czy pojawi się komunikat "E-mail w tej domenie nie jest dozwolony przy zakładaniu konta na OLX. Spróbuj jeszcze raz przy użyciu innego e-maila lub skontaktuj się z naszym Centrum pomocy."
+            var wrongDomain = _webDriver.FindElement(By.CssSelector(_errorMessageSelector));
+            StringAssert.Contains("E-mail w tej domenie nie jest dozwolony przy zakładaniu konta na OLX. Spróbuj jeszcze raz przy użyciu innego e-maila lub skontaktuj się z naszym Centrum pomocy.", wrongDomain.Text);
+        }
+        
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Brak loginu i hasła - niepoprawne logowanie na konto użytkownika")]
+        public void CorrectLogInTestMW()
+        {
+            ClickAcceptContainer();
+
+            ClickMyAccount();
+
+            WaitForAction(_logInFormSelector);
+
+            //kliknać "Zaloguj się"
+            var logInButton = _webDriver.FindElement(By.CssSelector(_logInButtonSelector));
+            logInButton.Click();
+
+            //sprawdzić, czy pojawi się komunikat "To pole jest wymagane"
+            var emptyPasswordInput = _webDriver.FindElement(By.CssSelector(_errorMessageSelector));
+            StringAssert.Contains("To pole jest wymagane", emptyPasswordInput.Text);
+        }
+
+        private void ClickMyAccount()
+        {
+            var loginLink = _webDriver.FindElement(By.CssSelector(_myAccountLinkSelector));
+            loginLink.Click();
+        }
+    }
+}
