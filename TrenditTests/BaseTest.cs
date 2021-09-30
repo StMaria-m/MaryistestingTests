db2641a4f9120e2d8eb653d3e8ee78525efe1f53
StMaria-m <stmaria-m@github>
2021-09-30 22:04:10 +0200

    Create TrenditTests project and add LogInTests

---
+++ TrenditTests/BaseTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrenditTests.Enums;
+
+namespace TrenditTests
+{
+    public class BaseTest
+    {
+        protected IWebDriver _webDriver;
+        protected string _path;
+
+        [SetUp]
+        public void Open()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Url = $"https://www.xxx.pl/{_path}";
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            _webDriver.Quit();
+        }
+
+        public void WaitForElementDisplayed(string selector, SearchByTypeEnums selectorType = SearchByTypeEnums.CssSelector)
+        {
+            var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => {
+                var selectorFunction = selectorType == SearchByTypeEnums.CssSelector ? By.CssSelector(selector) : By.XPath(selector);
+
+                var element = driver.FindElements(selectorFunction).FirstOrDefault();
+                if (element == null)
+                {
+                    return false;
+                }
+                return element.Displayed;
+            });
+        }      
+
+        public void LogInUserAccount()
+        {
+            //kliknąć przycisk Zaloguj
+            var logInButton = _webDriver.FindElement(By.CssSelector("a[href='https://panel.xxx.pl']"));
+            logInButton.Click();
+
+            WaitForElementDisplayed("[name=login]");
+
+            //uzupełnić Login
+            var loginInput = _webDriver.FindElement(By.CssSelector("[name=login]"));
+            loginInput.SendKeys("");
+
+            //uzupłnić hasło
+            var passwordInput = _webDriver.FindElement(By.CssSelector("[name=password]"));
+            passwordInput.SendKeys("");
+
+            //kliknąć przycisk "Zaloguj się"
+            var registrationButton = _webDriver.FindElement(By.CssSelector("[type=submit].button100"));
+            registrationButton.Click();
+
+            WaitForElementDisplayed("a[href='/wyloguj']");
+        }       
+    }
+}
---
+++ TrenditTests/LogInTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace TrenditTests
+{
+    public class LogInTests : BaseTest
+    {
+        string submitButton = "[type=submit].button100";
+        string loginSelector = "[name=login]";
+        string wrongLoginInput = "mtest@vp.pl";
+        string errorMessageSelector = "#formLogin span.error";
+        string loginInput = "";
+        string passwordSelector = "[name=password]";
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Logowanie bez loginu i hasła")]
+        public void IncorrectLoginTest_noLoginAndNoPassword()
+        {
+            GoToLoginPage();
+
+            //kliknąć przycisk "Zaloguj się"
+            _webDriver.FindElement(By.CssSelector(submitButton)).Click();
+
+            //Sprawdzenie, czy pojawią się dwa komunikaty "Niepoprawny login lub hasło."
+            var noData = _webDriver.FindElements(By.CssSelector(errorMessageSelector));
+            Assert.AreEqual(2, noData.Count);
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Błędne login i haslo - logowanie")]
+        public void IncorrectLoginTest_wrongLoginAndPasswordLogInTest()
+        {
+            GoToLoginPage();
+
+            //uzupełnić Login
+            _webDriver.FindElement(By.CssSelector(loginSelector)).SendKeys(wrongLoginInput);
+
+            //uzupełnić hasło
+            _webDriver.FindElement(By.CssSelector(passwordSelector)).SendKeys("1111");
+
+            //kliknąć przycisk "Zaloguj się"
+            _webDriver.FindElement(By.CssSelector(submitButton)).Click();
+
+            //Sprawdzenie, czy pojawią się dwa komunikaty "Niepoprawny login lub hasło."   
+            var noData = _webDriver.FindElements(By.CssSelector(errorMessageSelector));
+            Assert.AreEqual(2, noData.Count);
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Błędne haslo - logowanie")]
+        public void IncorrectLoginTest_noPasswordLogInTest()
+        {
+            string errorPasswordMessageSelector = "[id='password.error']";
+
+            GoToLoginPage();
+
+            //uzupełnić Login
+            _webDriver.FindElement(By.CssSelector(loginSelector)).SendKeys(loginInput);
+
+            //kliknąć przycisk "Zaloguj się"
+            _webDriver.FindElement(By.CssSelector(submitButton)).Click();
+
+            WaitForElementDisplayed(errorPasswordMessageSelector);
+
+            //Sprawdzenie, czy pojawi się komunikat "Pole jest wymagane."   
+            var noPasswordMessage = _webDriver.FindElements(By.CssSelector(errorPasswordMessageSelector));
+            Assert.IsTrue(noPasswordMessage.Count == 1);
+        }
+
+        [Test]
+        [Category("User log in tests")]
+        [Author("Maria", "http://maryistesting.com")]
+        [Description("Poprawne logowanie")]
+        public void LogInTest()
+        {
+            LogInUserAccount();
+
+            //sprawdzenie
+            var logOutButton = _webDriver.FindElements(By.CssSelector("a[href='/wyloguj']"));
+            Assert.IsTrue(logOutButton.Count > 0);
+        }
+
+        private void GoToLoginPage() => _webDriver.Navigate().GoToUrl($"https://panel.xxx.pl/logowanie");
+    }
+}
