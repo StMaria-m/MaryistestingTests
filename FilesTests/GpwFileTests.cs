9f3869be6632e72d15e4983c8eafb3309f6f60dd
StMaria-m <stmaria-m@github>
2021-12-01 21:51:01 +0100

    Add GpwFileTests

---
+++ FilesTests/GpwFileTests.cs
@@ -0,0 +1,46 @@
+﻿using ApiTests;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Linq;
+using FilesTests.Models;
+
+namespace FilesTests
+{
+    public class GpwFileTests
+    {
+        [Test]
+        public void CheckIfNamesAreUnique()
+        {
+            var akcjeLista = GetDataFromFile();
+
+            List<string> nazwyLista = new List<string>();
+
+            foreach (var akcja in akcjeLista)
+            {
+                nazwyLista.Add(akcja.Nazwa);
+            }
+
+            Assert.AreEqual(nazwyLista.GroupBy(i => i).Count(), nazwyLista.Count);
+        }
+
+        [Test]
+        public void CheckIfShortsAreUniqu5e()
+        {
+            var akcjeLista = GetDataFromFile();
+
+            Assert.AreEqual(akcjeLista.GroupBy(i => i.Skrot).Count(), akcjeLista.Count);
+        }
+
+        private IList<GpwModel> GetDataFromFile()
+        {
+            var dataString = File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\Data//GpwData.json");
+            return JsonConvert.DeserializeObject<IList<GpwModel>>(dataString);
+        }
+
+    }
+}
---
+++ FilesTests/Models/GpwModel.cs
@@ -0,0 +1,27 @@
+﻿namespace FilesTests.Models
+{
+    public class GpwModel
+    {
+        public string Nazwa { get; set; }
+        public string Skrot { get; set; }
+        public string Waluta { get; set; }
+        public string CzasOstatniejTransakcji { get; set; }
+        public string KursOdniesienia { get; set; }
+        public string TKO { get; set; }
+        public string KursOtwarcia { get; set; }
+        public string KursMin { get; set; }
+        public string KursMax { get; set; }
+        public string KursOstTransZamkn { get; set; }
+        public string ZmianaDoKursuOdn { get; set; }
+        public string KupnoLiczbaZlecen { get; set; }
+        public string KupnoWolumen { get; set; }
+        public string KupnoLimit { get; set; }
+        public string SprzedazLimit { get; set; }
+        public string SprzedazWolumen { get; set; }
+        public string SprzedazLiczbaZlecen { get; set; }
+        public string WolumenOstTrans { get; set; }
+        public string LiczbaTransakcji { get; set; }
+        public string WolObrSkumul { get; set; }
+        public string WartObrSkumul { get; set; }
+    }
+}
